0026b64b35c1c72d445400b98ad5d9221154d758
Sadik <anarul6777@gmail.com>
2023-07-22 08:56:59 +0600

    intial setup is done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className="rounded-full ...">Save Changes</button>
+      <h2>Bangladesh</h2>
+    </>
+  )
+}
+
+export default App
